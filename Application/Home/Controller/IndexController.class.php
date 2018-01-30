97a7d87e0772d8600514a077f699f910382c58fa
2497987050 <2497987050@qq.com>
2018-01-30 14:02:09 +0800

    控制器
    returnLngLatByLocation 方法优化，
    及新增getInfoByLocation

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -54,15 +54,48 @@ class IndexController extends controller {
     //根据地名返回对应的经纬度
     public function returnLngLatByLocation(){
     	$address = !empty($_GET['location']) ? $_GET['location']:'北京市';
-    	
-    	$sk = 'B28SnTg2B6Cdw2GfaYq4KUAsfFpe7H1f';
-		$url = "http://api.map.baidu.com/geocoder/v2/?address=%s&output=json&ak=4vWwiDYUhYY3S5uLoPbZqUEz&sn=%s";
-		$querystring = "address=".$address."&output=json&ak=4vWwiDYUhYY3S5uLoPbZqUEz";  
-	    $sn =  md5(urlencode('/geocoder/v2/?'.$querystring.$sk));  
-		$target = sprintf($url, urlencode($address), $sn);
-		$html = json_decode(file_get_contents($target),true);
-		
-		$this->ajaxReturn($html,'JSON');
+		$return = $this->lonLat($address);
+//		$html = json_decode($return,true);
+		$this->ajaxReturn($return,'JSON');
     }
     
+    public function lonLat($arName){
+    	$select = D('area_lng_lat')->field('lng,lat')->where("area_name = '".$arName."'")->select();
+    	if($select){
+    		return $select[0];
+    	}else{
+    		$sk = 'B28SnTg2B6Cdw2GfaYq4KUAsfFpe7H1f';
+			$url = "http://api.map.baidu.com/geocoder/v2/?address=%s&output=json&ak=4vWwiDYUhYY3S5uLoPbZqUEz&sn=%s";
+			$querystring = "address=".$arName."&output=json&ak=4vWwiDYUhYY3S5uLoPbZqUEz";
+		    $sn =  md5(urlencode('/geocoder/v2/?'.$querystring.$sk));  
+			$target = sprintf($url, urlencode($arName), $sn);
+			$html = json_decode(file_get_contents($target),true);
+			$html['result']['location']['area_name']=$arName;
+			$html['result']['location']['level']=$html['result']['level'];
+			
+			$insert = M("area_lng_lat")->data($html['result']['location'])->add();
+			
+			return $html['result']['location'];
+    	}
+    	
+    	
+    }
+    //传递地名参数，获取对应参数下的行业
+    public function getInfoByLocation(){
+    	if(!empty($_GET['location'])){
+    		$infos = D('company_distribution')->field('*')->where("province = '".$_GET['location']."'")->select();
+    		
+    	}else{
+    		$infos = D('company_distribution')->field('province,SUM(company_no) AS company_no,SUM(compnay_IBno) AS compnay_IBno,SUM(electricity) AS electricity,SUM(serviceProvider) AS serviceProvider,SUM(internet) AS internet,SUM(computer) AS computer,SUM(transportation) AS transportation,SUM(financialServices) AS financialServices,SUM(researchEducation) AS researchEducation,SUM(retail) AS retail,SUM(media) AS media,SUM(energyUtilities) AS energyUtilities,SUM(wholesaleDistribution) AS wholesaleDistribution,SUM(medical) AS medical,SUM(entertainment) AS entertainment,SUM(government) AS government,SUM(manufacturing) AS manufacturing,SUM(professionalServices) AS professionalServices,SUM(Others) AS Others')->group('province')->select();
+    	}
+    	for($i=0;$i<count($infos);$i++){
+    		$local = ($infos[$i]['city'])?$infos[$i]['city']:$infos[$i]['province'];
+    		$return = $this->lonLat($local);
+    		$infos[$i]['lng'] =$return["lng"];
+    		$infos[$i]['lat'] =$return["lat"];
+    	}
+//  	echo "<pre>";
+//  	print_r($infos);
+    	$this->ajaxReturn($infos,'JSON');
+    }
 }
