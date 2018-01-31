4e20f81cd3ac1a71c328dbe8035fd77370905aea
flawZhang <18356055044@163.com>
2018-01-31 11:19:49 +0800

    人口密度和公司分布

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -12,16 +12,41 @@ class IndexController extends controller {
     }
     //2018.01.25 @young 人口密度
     public function macro(){
-    	$model = D('macrodata')->distinct(true)->field('province')->select();
-    	
-    	$model = D('macrodata')->where("province = '河南省'")->sum('poptotal');
-    	var_dump($model);
+    	$model = D('macrodata')->field('province,sum(poptotal) as poptotal,sum(area) as area')->group('province')->select();
+       /* for ($i=0; $i < count($model); $i++) { 
+            $model[$i]['areaPop'] = $model[$i]['poptotal']/$model[$i]['area'];
+        }
+        header('content-type:text/html;charset=utf-8');
+    	var_dump($model);*/
     	$this->ajaxReturn($model,'JSON');
+    }
+    public function macroCity(){
+        $type = I('post.showLi');
+        //var_dump($type);
+        if ($type == '宁夏') {
+            $type = '宁夏回族自治区';
+        }
+        if ($type == '广西') {
+            $type = '广西壮族自治区';
+        }
+        if ($type == '新疆') {
+            $type = '新疆维吾尔自治区';
+        }
+        if ($type == '西藏') {
+            $type = '西藏自治区';
+        }
+        $model = D('macrodata')->field('city,sum(poptotal) as poptotal,sum(area) as area')->where("province = '$type'")->group('city')->select();
+       /* for ($i=0; $i < count($model); $i++) { 
+            $model[$i]['areaPop'] = $model[$i]['poptotal']/$model[$i]['area'];
+        }
+        header('content-type:text/html;charset=utf-8');
+        var_dump($model);*/
+        $this->ajaxReturn($model,'JSON');
     }
     //2018.01.25 @young 省份公司分布
     public function company(){
     	$type = I('post.valShow');
-        $dd =$infos = D('company_distribution')->field('province,SUM(company_no) AS company_no,count(compnay_IBno) AS compnay_IBno,count(RS_20_cities) as rs20,count(OS_20_cities) as os20,count(OS_64_cities) as os64,count(OS_108_cities) as os108')->/*where("RS_20_cities = 'Y' or OS_20_cities = 'Y' or OS_64_cities = 'Y' or OS_108_cities = 'Y'")->*/group('province')->select();
+        $dd =$infos = D('company_distribution')->field('province,SUM(company_no) AS company_no,count(compnay_IBno) AS compnay_IBno,count(RS_20_cities="Y" or null) as rs20,count(OS_20_cities="Y" or null) as os20,count(OS_64_cities="Y" or null) as os64,count(OS_108_cities="Y" or null) as os108')->group('province')->select();
        /*header('content-type:text/html;charset=utf-8');
         var_dump($dd);*/
     
@@ -44,7 +69,7 @@ class IndexController extends controller {
             $type = '西藏自治区';
         }
         //$city = D('company_distribution')->distinct(true)->field('city')->where("province = '$type'")->select();
-        $city =$infos = D('company_distribution')->field('city,SUM(company_no) AS company_no,SUM(compnay_IBno) AS compnay_IBno')->where("province = '$type'")->group('city')->select();
+        $city =$infos = D('company_distribution')->field('city,SUM(company_no) AS company_no,SUM(compnay_IBno) AS compnay_IBno,count(compnay_IBno) AS compnay_IBno,count(RS_20_cities="Y" or null) as rs20,count(OS_20_cities="Y" or null) as os20,count(OS_64_cities="Y" or null) as os64,count(OS_108_cities="Y" or null) as os108')->where("province = '$type'")->group('city')->select();
        /* header('content-type:text/html;charset=utf-8');
         echo '<pre>';
         var_dump($city);die();
---
+++ Public/js/macrodata.js
@@ -0,0 +1,102 @@
+//省份
+	function getMacro(data){       
+		var bdary = new BMap.Boundary();
+		bdary.get(data.province, function(rs){       //获取行政区域
+			//map.clearOverlays();        //清除地图覆盖物   
+			var count = rs.boundaries.length; //行政区域的点有多少个
+			var val=$('input:radio[name="doc-radio-2"]:checked').val();
+			var num = 0.001;
+			for (var i = 0; i < count; i++) {
+				/*if (val == '人口密度') {
+					data.company_no=data.compnay_IBno;
+					var num = 0.02;
+				};*/
+				
+				var ply = new BMap.Polygon(rs.boundaries[i], {strokeWeight: 0.5, strokeColor: "#333",fillColor:"#B30000",fillOpacity:data.popArea*num}); //建立多边形覆盖物
+				map.addOverlay(ply);  //添加覆盖物	
+		
+			} 
+			
+		});   
+
+	}
+	//城市
+	 function macroCity(data){       
+		var bdary = new BMap.Boundary();
+		bdary.get(data.city, function(rs){       //获取行政区域
+			//map.clearOverlays();        //清除地图覆盖物   
+			var count = rs.boundaries.length; //行政区域的点有多少个
+			var num = 0.0009;
+			var val=$('input:radio[name="doc-radio-2"]:checked').val();
+			for (var i = 0; i < count; i++) {
+				if (val == '装机公司数量') {
+					data.company_no=data.compnay_IBno;
+					var num = 0.2;
+				};
+				var ply = new BMap.Polygon(rs.boundaries[i], {strokeWeight: 0.5, strokeColor: "#333",fillColor:"#B30000",fillOpacity:data.popArea*num}); //建立多边形覆盖物
+				map.addOverlay(ply);  //添加覆盖物	
+			} 
+			map.centerAndZoom(data.city, 8);   //调整视野   
+		});   
+
+	}
+	$('#macroDiv input').click(function(){
+		//console.log(11);
+		map.clearOverlays();        //清除地图覆盖物   
+		var valShow = $(this).val();
+			$.ajax({
+		        type: "post",
+		        url: "index/macro",
+		        data:{valShow:valShow},
+		        success:function(e){
+		            var datas;
+		            datas = e;
+		            //console.log(datas);
+		            for(var i=0;i<datas.length;i++){  
+		            var popArea = datas[i]['poptotal']/datas[i]['area'];
+						popArea = popArea.toFixed(2);
+						datas[i]['popArea'] = popArea;  
+						getMacro(datas[i]); 
+						
+						//console.log(popArea);
+						if (valShow == '人口密度') {
+							searchByStationName(datas[i]['province'],datas[i]['popArea']);	
+						};
+						
+					} 
+		        }
+			});  
+			//城市
+			$('.showUl a').click(function(){
+				map.clearOverlays();        //清除地图覆盖物   
+				var showLi = $.trim($(this).first().text());
+				var val=$('input:radio[name="doc-radio-2"]:checked').val();
+				console.log(val);
+				$.ajax({
+			        type: "post",
+			        url: "Index/macroCity",
+			        data:{showLi:showLi},
+			        success:function(e){
+			            var datas;
+			            datas = e;
+			            if (typeof(val) == 'undefined') {
+			            		alert('请选择指标！');
+			            }else{
+			            	 for(var i=0;i<datas.length;i++){ 
+			            	 	var popArea = datas[i]['poptotal']/datas[i]['area'];
+								popArea = popArea.toFixed(2);
+								datas[i]['popArea'] = popArea;  
+			            	 	macroCity(datas[i]);
+								if (val == '人口密度') {
+									searchByStationName(datas[i]['city'],datas[i]['popArea']);
+								};
+								
+							}
+			            }
+			           
+			        }
+				});  
+			});
+	});
+
+	
